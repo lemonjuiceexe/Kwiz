1b993f5bc9b051fba6525661bb1874c4dcf2fa47
Bruhtek <bartosz.kas2005@gmail.com>
2022-01-30 18:34:33 +0100

    Add questions, add math round for percents

--- js/main.js
+++ js/main.js
@@ -27,9 +27,10 @@ let correctCount = 0;
 let incorrectCount = 0;
 let index = 0;
 
-function init() {
+function init()
+{
 	index = 0; correctCount = 0; incorrectCount = 0;
-	
+
 	container1.style.display = "none";
 	container2.style.display = "flex";
 	container3.style.display = "none";
@@ -40,14 +41,16 @@ function init() {
 
 init();
 
-function displayAuthors() {
+function displayAuthors()
+{
 	container1.style.display = "none";
 	container2.style.display = "none";
 	container3.style.display = "flex";
 	container4.style.display = "none";
 }
 
-function startKwiz() {
+function startKwiz()
+{
 	index = 0;
 	container1.style.display = "grid";
 	container2.style.display = "none";
@@ -61,28 +64,34 @@ function startKwiz() {
 
 let timerPause = false;
 
-async function ans(x) {
-	if(timerPause){return;}
-	if (x == questions[index]["c"]) {
+async function ans(x)
+{
+	if (timerPause) { return; }
+	if (x == questions[index]["c"])
+	{
 		correct();
 	}
-	else {
+	else
+	{
 		incorrect();
 	}
 	timerPause = true;
-	
-	if (x >= 0 && x <= 3) {
+
+	if (x >= 0 && x <= 3)
+	{
 		highlightQuestion(x, x == questions[index]["c"]);
 		highlightQuestion(questions[index]["c"], true);
-	} else {
+	} else
+	{
 		highlightQuestion(questions[index]["c"], false);
 	}
-	
+
 	//Pause script for 3 seconds
 	await new Promise(r => setTimeout(r, 3000));
-	
+
 	++index;
-	if (index >= questions.length) {
+	if (index >= questions.length)
+	{
 		endTheSuffering();
 		return;
 	}
@@ -90,77 +99,92 @@ async function ans(x) {
 	nextQuestion();
 }
 
-async function endTheSuffering() {
+async function endTheSuffering()
+{
 	container1.style.display = "none";
 	container2.style.display = "none";
 	container3.style.display = "none";
 	container4.style.display = "flex";
 	barScore.style.display = "flex";
 	barTime.style.display = "none";
-	
+
 	scoreInfo.innerHTML = "Zdobyłeś <span class=\"green\">" + correctCount + "</span> na <span class=\"red\">" + questions.length + "</span> punktów!";
-	percentInfo.innerHTML = "Daje to " + "<span class=\"blue\">" + (correctCount/questions.length)*100 + "%</span>!"
-	
+	percentInfo.innerHTML = "Daje to " + "<span class=\"blue\">" + Math.round(correctCount / questions.length * 100) + "%</span>!"
+
 	let wordsSpan = document.getElementsByClassName("words-span");
-	for (let i = 0; i < 4; i++) {
+	for (let i = 0; i < 4; i++)
+	{
 		wordsSpan[i].style.animation = "words 10s infinite";
 	}
-	
+
 	word1.innerHTML = "w trąbkę";
 	word2.innerHTML = words[Math.floor(Math.random() * words.length)];
-	
-	let interval = setInterval(() => {
+
+	let interval = setInterval(() =>
+	{
 		word1.innerHTML = words[Math.floor(Math.random() * words.length)];
 		word2.innerHTML = words[Math.floor(Math.random() * words.length)];
 	}, 10000)
 }
 
-function highlightQuestion(ansNumber, correct) {
-	if (correct) {
+function highlightQuestion(ansNumber, correct)
+{
+	if (correct)
+	{
 		answers[ansNumber].style.border = "10px solid #9ece6a";
 		return;
 	}
 	answers[ansNumber].style.border = "10px solid #f7768e";
 }
-function uncolorQuestions(){
-	for(let i = 0; i < answers.length; i++){answers[i].style.border="none";}
+function uncolorQuestions()
+{
+	for (let i = 0; i < answers.length; i++) { answers[i].style.border = "none"; }
 }
 
-function nextQuestion() {
+function nextQuestion()
+{
 	q.innerHTML = questions[index]["q"];
-	for (let i = 0; i < 4; i++) {
+	for (let i = 0; i < 4; i++)
+	{
 		ansp[i].innerHTML = questions[index][i];
 	}
 	uncolorQuestions();
-	
+
 	timerPause = false;
 	tbar.classList.remove("timer-animation");
 	void tbar.offsetWidth;
 	tbar.classList.add("timer-animation");
 	tbar.style.animationPlayState = "running";
-	
+
 	timer();
 }
 
 let width = 0;
-function timer() {
+function timer()
+{
 	width = 90;
-	var id = setInterval(() => {
-		if (timerPause) { 
+	var id = setInterval(() =>
+	{
+		if (timerPause)
+		{
 			clearInterval(id);
 			tbar.style.animationPlayState = "paused";
 			return;
 		}
-			
-		if (width <= 0) {
+
+		if (width <= 0)
+		{
 			clearInterval(id);
 			ans(-1);
-		} else {
+		} else
+		{
 			width--;
-			if (width / 10 >= 1) {
+			if (width / 10 >= 1)
+			{
 				tlabel.innerHTML = Math.round(width / (9 / 10) / 10) + " s";
 			}
-			else {
+			else
+			{
 				tlabel.innerHTML = Math.round((width / (9 / 10))) / 10 + " s";
 			}
 		}
@@ -170,18 +194,21 @@ function timer() {
 	tbar.classList.add("timer-animation");
 }
 
-function incorrect() {
+function incorrect()
+{
 	incorrectCount++;
 	updateScore();
 }
-function correct() {
+function correct()
+{
 	correctCount++;
 	updateScore();
 }
 
-function updateScore() {
+function updateScore()
+{
 	correctLabel.innerHTML = correctCount;
 	wrongLabel.innerHTML = incorrectCount;
-	cbar.style.width = ((100/questions.length) * correctCount)+"%";
-	ibar.style.width = ((100/questions.length) * incorrectCount)+"%";
+	cbar.style.width = ((100 / questions.length) * correctCount) + "%";
+	ibar.style.width = ((100 / questions.length) * incorrectCount) + "%";
 }
